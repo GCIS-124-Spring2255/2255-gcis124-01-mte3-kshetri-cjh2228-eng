0dde270be1eb08f73109726ef39ad06e0b4df4b5
Carter Hill (RIT Student) <cjh2228@rit.edu>
2026-04-06 10:32:13 -0400

    question 1

--- src/main/java/mte3/tempconvert/Converter.java
+++ src/main/java/mte3/tempconvert/Converter.java
@@ -13,18 +13,32 @@ public class Converter {
         
     } // CelciusToFahrenheit closed
 
+    
+
     public static void main(String[] args) {
         
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("Please enter temperature (for conversion): ");
 
             // (part 1) conversion from C to F
+            double value = scanner.nextDouble();
+            TempConvert celsiusToFahrenheit = new CelsiusToFahrenheit();
+            System.out.println("C to F: " + celsiusToFahrenheit.convert(value));
             
             
             // (part 2) conversion from F to C
+            TempConvert cToF = new TempConvert() {
+                @Override
+                public double convert(double f) {
+                    return (f-32)*5.0/9.0;
+                }
+            };
+            System.out.println("CtoF: " + cToF.convert(value));
             
             
             // (part 3) conversion from F to K
+            TempConvert FToK = f -> (f-32)*5.0/9.0+273.15; 
+            System.out.println("FtoK: " + FToK.convert(value));
             
 
 
